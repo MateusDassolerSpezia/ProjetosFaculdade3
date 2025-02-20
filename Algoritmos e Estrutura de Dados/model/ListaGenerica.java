9500e88096de547ef3919fce59270edbc8bdaa6e
MateusDassolerSpezia <163303039+MateusDassolerSpezia@users.noreply.github.com>
2025-02-20 11:09:14 -0300

    Add files via upload

---
+++ Algoritmos e Estrutura de Dados/model/ListaGenerica.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author mdspezia
+ */
+public class ListaGenerica<T> {
+    
+    private T info;
+
+    public T getInfo() {
+        return info;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+    
+    
+}
