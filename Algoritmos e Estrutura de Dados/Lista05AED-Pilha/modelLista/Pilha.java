cdc7243223ff89458154f1d3b815167ce75e66c9
MateusDassolerSpezia <163303039+MateusDassolerSpezia@users.noreply.github.com>
2025-04-10 08:41:59 -0300

    Update Pilha.java

--- Algoritmos e Estrutura de Dados/Lista05AED-Pilha/modelLista/Pilha.java
+++ Algoritmos e Estrutura de Dados/Lista05AED-Pilha/modelLista/Pilha.java
@@ -11,9 +11,9 @@ package modelLista;
  */
 public interface Pilha<T> {
     
-    public void push(T info);
-    public T pop();
-    public T peek();
-    public boolean estaVazia();
-    public void liberar();
+    void push(T info);
+    T pop();
+    T peek();
+    boolean estaVazia();
+    void liberar();
 }
