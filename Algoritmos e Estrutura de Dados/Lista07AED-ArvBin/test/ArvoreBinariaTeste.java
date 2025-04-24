e9da4a42c9ac07dee722109ddd458504e3fee5f9
MateusDassolerSpezia <163303039+MateusDassolerSpezia@users.noreply.github.com>
2025-04-23 21:40:07 -0300

    Add files via upload

---
+++ Algoritmos e Estrutura de Dados/Lista07AED-ArvBin/test/ArvoreBinariaTeste.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package test;
+
+import model.ArvoreBinaria;
+import model.NoArvoreBinaria;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Mateus
+ */
+public class ArvoreBinariaTeste {
+   
+    private static ArvoreBinaria arv;
+    
+    @BeforeEach
+    public void setUp() {
+        arv = new ArvoreBinaria();
+    }
+    
+    @Test
+    public void Teste01() {
+        assertTrue(arv.estaVazia());
+    }
+    
+    @Test
+    public void Teste02() {
+        NoArvoreBinaria no1 = new NoArvoreBinaria(5);
+        arv.setRaiz(no1);
+        assertFalse(arv.estaVazia());
+    }
+    
+    @Test
+    public void Teste03() {
+        NoArvoreBinaria no4 = new NoArvoreBinaria(4);
+        NoArvoreBinaria no2 = new NoArvoreBinaria(2, null, no4);
+        NoArvoreBinaria no5 = new NoArvoreBinaria(5);
+        NoArvoreBinaria no6 = new NoArvoreBinaria(6);
+        NoArvoreBinaria no3 = new NoArvoreBinaria(3, no5, no6);
+        NoArvoreBinaria no1 = new NoArvoreBinaria(1, no2, no3);
+        arv.setRaiz(no1);
+        
+    }
+    
+    @Test
+    public void Teste04() {
+        NoArvoreBinaria no4 = new NoArvoreBinaria(4);
+        NoArvoreBinaria no2 = new NoArvoreBinaria(2, null, no4);
+        NoArvoreBinaria no5 = new NoArvoreBinaria(5);
+        NoArvoreBinaria no6 = new NoArvoreBinaria(6);
+        NoArvoreBinaria no3 = new NoArvoreBinaria(3, no5, no6);
+        NoArvoreBinaria no1 = new NoArvoreBinaria(1, no2, no3);
+        arv.setRaiz(no1);
+        assertTrue(arv.pertence(1));
+    }
+    
+    @Test
+    public void Teste05() {
+        NoArvoreBinaria no4 = new NoArvoreBinaria(4);
+        NoArvoreBinaria no2 = new NoArvoreBinaria(2, null, no4);
+        NoArvoreBinaria no5 = new NoArvoreBinaria(5);
+        NoArvoreBinaria no6 = new NoArvoreBinaria(6);
+        NoArvoreBinaria no3 = new NoArvoreBinaria(3, no5, no6);
+        NoArvoreBinaria no1 = new NoArvoreBinaria(1, no2, no3);
+        arv.setRaiz(no1);
+        assertTrue(arv.pertence(3));
+    }
+    
+    @Test
+    public void Teste06() {
+        NoArvoreBinaria no4 = new NoArvoreBinaria(4);
+        NoArvoreBinaria no2 = new NoArvoreBinaria(2, null, no4);
+        NoArvoreBinaria no5 = new NoArvoreBinaria(5);
+        NoArvoreBinaria no6 = new NoArvoreBinaria(6);
+        NoArvoreBinaria no3 = new NoArvoreBinaria(3, no5, no6);
+        NoArvoreBinaria no1 = new NoArvoreBinaria(1, no2, no3);
+        arv.setRaiz(no1);
+        assertTrue(arv.pertence(6));
+    }
+    
+    @Test
+    public void Teste07() {
+        NoArvoreBinaria no4 = new NoArvoreBinaria(4);
+        NoArvoreBinaria no2 = new NoArvoreBinaria(2, null, no4);
+        NoArvoreBinaria no5 = new NoArvoreBinaria(5);
+        NoArvoreBinaria no6 = new NoArvoreBinaria(6);
+        NoArvoreBinaria no3 = new NoArvoreBinaria(3, no5, no6);
+        NoArvoreBinaria no1 = new NoArvoreBinaria(1, no2, no3);
+        arv.setRaiz(no1);
+        assertFalse(arv.pertence(10));
+    }
+    
+    @Test
+    public void Teste08() {
+        NoArvoreBinaria no4 = new NoArvoreBinaria(4);
+        NoArvoreBinaria no2 = new NoArvoreBinaria(2, null, no4);
+        NoArvoreBinaria no5 = new NoArvoreBinaria(5);
+        NoArvoreBinaria no6 = new NoArvoreBinaria(6);
+        NoArvoreBinaria no3 = new NoArvoreBinaria(3, no5, no6);
+        NoArvoreBinaria no1 = new NoArvoreBinaria(1, no2, no3);
+        arv.setRaiz(no1);
+        assertEquals(arv.contarNos(), 6);
+    }
+}
