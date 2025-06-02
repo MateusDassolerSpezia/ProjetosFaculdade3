f8279a3c9b4c863eeeed0d192037d00b60a0a0ff
MateusDassolerSpezia <163303039+MateusDassolerSpezia@users.noreply.github.com>
2025-06-02 18:39:10 -0300

    Update MapaDispersaoTeste.java

--- Algoritmos e Estrutura de Dados/Lista09AED-HashMap/src/test/java/test/MapaDispersaoTeste.java
+++ Algoritmos e Estrutura de Dados/Lista09AED-HashMap/src/test/java/test/MapaDispersaoTeste.java
@@ -35,11 +35,47 @@ public class MapaDispersaoTeste {
     
     @Test
     public void Teste02() {
-        
+        Aluno ob1 = new Aluno(12000, "Jean", LocalDate.of(2000, 1, 1));
+        Aluno ob2 = new Aluno(14000, "Pedro", LocalDate.of(1999, 1, 20));
+        Aluno ob3 = new Aluno(12500, "Marta", LocalDate.of(2001, 2, 18));
+        Aluno ob4 = new Aluno(13000, "Lucas", LocalDate.of(1998, 11, 25));
+        mapa.inserir(12000, ob1);
+        mapa.inserir(14000, ob2);
+        mapa.inserir(12500, ob3);
+        mapa.inserir(13000, ob4);
+        assertTrue(mapa.buscar(12000).equals(ob1));
+        assertTrue(mapa.buscar(14000).equals(ob2));
+        assertTrue(mapa.buscar(12500).equals(ob3));
+        assertTrue(mapa.buscar(13000).equals(ob4));
     }
     
     @Test
     public void Teste03() {
-        
+        Aluno ob1 = new Aluno(12000, "Jean", LocalDate.of(2000, 1, 1));
+        Aluno ob2 = new Aluno(14000, "Pedro", LocalDate.of(1999, 1, 20));
+        Aluno ob3 = new Aluno(14226, "Marta", LocalDate.of(2001, 2, 18));
+        Aluno ob4 = new Aluno(17180, "Lucas", LocalDate.of(1998, 11, 25));
+        mapa.inserir(12000, ob1);
+        mapa.inserir(14000, ob2);
+        mapa.inserir(14226, ob3);
+        mapa.inserir(17180, ob4);
+        assertTrue(mapa.buscar(12000).equals(ob1));
+        assertTrue(mapa.buscar(14000).equals(ob2));
+        assertTrue(mapa.buscar(14226).equals(ob3));
+        assertTrue(mapa.buscar(17180).equals(ob4));
+    }
+    
+    @Test
+    public void Teste04() {
+        Aluno ob1 = new Aluno(12000, "Jean", LocalDate.of(2000, 1, 1));
+        Aluno ob2 = new Aluno(14000, "Pedro", LocalDate.of(1999, 1, 20));
+        Aluno ob3 = new Aluno(14226, "Marta", LocalDate.of(2001, 2, 18));
+        Aluno ob4 = new Aluno(17180, "Lucas", LocalDate.of(1998, 11, 25));
+        mapa.inserir(12000, ob1);
+        mapa.inserir(14000, ob2);
+        mapa.inserir(14226, ob3);
+        mapa.inserir(17180, ob4);
+        mapa.remover(12000);
+        assertNull(mapa.buscar(12000));
     }
 }
