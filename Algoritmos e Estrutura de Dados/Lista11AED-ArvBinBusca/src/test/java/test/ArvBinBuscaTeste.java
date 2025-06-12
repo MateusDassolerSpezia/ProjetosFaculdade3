8071ab7901cd31ddecd61dc0b2ceb6d93e15fb44
MateusDassolerSpezia <163303039+MateusDassolerSpezia@users.noreply.github.com>
2025-06-11 23:41:26 -0300

    Update ArvBinBuscaTeste.java

--- Algoritmos e Estrutura de Dados/Lista11AED-ArvBinBusca/src/test/java/test/ArvBinBuscaTeste.java
+++ Algoritmos e Estrutura de Dados/Lista11AED-ArvBinBusca/src/test/java/test/ArvBinBuscaTeste.java
@@ -4,6 +4,7 @@
  */
 package test;
 
+import model.ArvoreBinariaBusca;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,32 +14,70 @@ import static org.junit.jupiter.api.Assertions.*;
 
 /**
  *
- * @author mdspezia
+ * @author Mateus
  */
 public class ArvBinBuscaTeste {
     
-    public ArvBinBuscaTeste() {
-    }
-    
-    @BeforeAll
-    public static void setUpClass() {
-    }
-    
-    @AfterAll
-    public static void tearDownClass() {
-    }
+    static ArvoreBinariaBusca arvBinBus;
     
     @BeforeEach
     public void setUp() {
+        arvBinBus = new ArvoreBinariaBusca();
     }
     
-    @AfterEach
-    public void tearDown() {
+    @Test
+    public void Teste01() {
+        //ArvoreBinariaBusca arvBinBus = new ArvoreBinariaBusca();
+        arvBinBus.inserir(50);
+        arvBinBus.inserir(30);
+        arvBinBus.inserir(70);
+        arvBinBus.inserir(40);
+        arvBinBus.inserir(25);
+        arvBinBus.inserir(75);
+        arvBinBus.inserir(65);
+        arvBinBus.inserir(35);
+        arvBinBus.inserir(60);
+        assertEquals(arvBinBus.toString(), "<50<30<25<><>><40<35<><>><>>><70<65<60<><>><>><75<><>>>>");
     }
-
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+    
+    @Test
+    public void Teste02() {
+        arvBinBus.inserir(50);
+        arvBinBus.inserir(30);
+        arvBinBus.inserir(25);
+        arvBinBus.inserir(40);
+        arvBinBus.retirar(40);
+        assertEquals(arvBinBus.toString(), "<50<30<25<><>><>><>>");
+    }
+    
+    @Test
+    public void Teste03() {
+        arvBinBus.inserir(80);
+        arvBinBus.inserir(52);
+        arvBinBus.inserir(90);
+        arvBinBus.inserir(48);
+        arvBinBus.inserir(71);
+        arvBinBus.inserir(63);
+        arvBinBus.inserir(67);
+        arvBinBus.retirar(71);
+        assertEquals(arvBinBus.toString(), "<80<52<48<><>><63<><67<><>>>><90<><>>>");
+    }
+    
+    @Test
+    public void Teste04() {
+        arvBinBus.inserir(250);
+        arvBinBus.inserir(38);
+        arvBinBus.inserir(26);
+        arvBinBus.inserir(72);
+        arvBinBus.inserir(55);
+        arvBinBus.inserir(90);
+        arvBinBus.inserir(41);
+        arvBinBus.inserir(60);
+        arvBinBus.inserir(43);
+        arvBinBus.inserir(78);
+        arvBinBus.inserir(92);
+        arvBinBus.inserir(74);
+        arvBinBus.retirar(38);
+        assertEquals(arvBinBus.toString(), "<250<41<26<><>><72<55<43<><>><60<><>>><90<78<74<><>><>><92<><>>>>><>>");
+    }
 }
