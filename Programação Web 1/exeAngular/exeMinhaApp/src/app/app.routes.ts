f3f51722a2d9f6ace2062c112ed91547b010fd75
MateusDassolerSpezia <mdspezia@furb.br>
2025-06-14 00:43:45 -0300

    Avaliação 2 (PW1)

--- Programação Web 1/exeAngular/exeMinhaApp/src/app/app.routes.ts
+++ Programação Web 1/exeAngular/exeMinhaApp/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './component/home/home';
+import { Funcionarios } from './component/funcionarios/funcionarios';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'funcionarios',
+        component: Funcionarios
+    }
+];
